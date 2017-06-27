eacbd20139b14a6be5793789cd9c1ffeb4802d66
Barney Staddon <bstaddon@museumoflondon.org.uk>
2017-06-27 15:54:38 +0100

    Remove module req

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const mysql = require('mysql')
+//const mysql = require('mysql')
 const app = express()
-const sql = "SELECT * FROM `events` WHERE `eid` = 126608"
+//const sql = "SELECT * FROM `events` WHERE `eid` = 126608"
 
 /*
 
